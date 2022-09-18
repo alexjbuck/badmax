ee6a24a4aa00c67c6922d1e7319aeb0cbbdf712f
Alex Buck <alexander.j.buck@navy.mil>
2022-09-18 11:19:53 -0400

    site_url did not fix problem. Now adjusting importScripts url
    sites/badmax/ added to importScripts and modulePathPrefix

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-importScripts("src/libraries/workbox-v6.5.4/workbox-sw.js");
+importScripts("sites/badmax/src/libraries/workbox-v6.5.4/workbox-sw.js");
 
 workbox.setConfig({
-    modulePathPrefix: "/src/libraries/workbox-v6.5.4/",
+    modulePathPrefix: "sites/badmax/src/libraries/workbox-v6.5.4/",
 })
 
 const cacheName = 'BadMaxCache_v2'
